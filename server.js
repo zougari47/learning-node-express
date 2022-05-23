97a16d9c710bc83ce8df0bc51c815915130485a4
zougari47 <zougariahmed007@gmail.com>
2022-05-23 20:01:08 +0100

    hello express

--- server.js
+++ server.js
@@ -1,109 +1,34 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-
-const logEvents = require('./logEvents');
-const EventEmitter = require('events');
-const { json } = require('stream/consumers');
-class Emitter extends EventEmitter {}
-// initialize object
-const myEmitter = new Emitter();
-myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
-
 const PORT = process.env.PORT || 3500;
-const serveFile = async (filePath, contentType, response) => {
-  //NOTE
-  try {
-    const rawData = await fsPromises.readFil(
-      filePath,
-      !contentType.includes('image') ? 'utf-8' : ''
-    );
-    const data =
-      contentType === 'application/json' ? JSON.parse(rawData) : rawData;
-    response.writeHead(filePath.includes('404.html') ? 404 : 200, {
-      'Content-Type': contentType
-    });
-    response.end(
-      contentType === 'application/json' ? JSON.stringify(data) : data
-    );
-  } catch (err) {
-    console.error(err);
-    myEmitter.emit('log', `${err.name}\t${err.message}`, 'errLog.txt');
-    response.statusCode = 500;
-    response.end();
-  }
-};
 
-const server = http.createServer((req, res) => {
-  // body
-  console.log(req.url, req.method);
-  myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
-
-  const extension = path.extname(req.url);
-
-  let contentType;
-
-  switch (extension) {
-    case '.css':
-      contentType = 'text/css';
-      break;
-    case '.js':
-      contentType = 'text/javascript';
-      break;
-    case '.json':
-      contentType = 'application/json';
-      break;
-    case '.jpg':
-      contentType = 'image/jpeg';
-      break;
-    case '.png':
-      contentType = 'image/png';
-      break;
-    case '.txt':
-      contentType = 'text/plain';
-      break;
-    default:
-      contentType = 'text/html';
-  }
-
-  let filePath =
-    contentType === 'text/html' && req.url === '/'
-      ? path.join(__dirname, 'views', 'index.html')
-      : contentType === 'text/html' && req.url.slice(-1) === '/'
-      ? path.join(__dirname, 'views', req.url, 'index.html')
-      : contentType === 'text/html'
-      ? path.join(__dirname, 'views', req.url)
-      : path.join(__dirname, req.url);
-
-  // makes .html extension not required in the browser
-  if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
-
-  const fileExists = fs.existsSync(filePath);
-
-  if (fileExists) {
-    // server the file
-    serveFile(filePath, contentType, res);
-  } else {
-    // 404
-    // 303 redirect
-    switch (path.parse(filePath).base) {
-      case 'old-page.html':
-        res.writeHead(301, { location: '/new-page.html' });
-        res.end();
-        break;
-
-      case 'www-page.html':
-        res.writeHead(301, { location: '/' });
-        res.end();
-
-      default:
-        // serve a 404 Response
-        serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
-    }
-  }
+app.get('^/$|/index(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-server.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+app.get('/new-page(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
 });
+
+app.get('/old-page(.html)?', (req, res) => {
+  res.redirect(301, '/new-page.html'); //302 by default
+});
+
+// Route handlers
+app.get(
+  '/hello(.html)?',
+  (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next();
+  },
+  (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', './'));
+    res.send('Hello World');
+  }
+);
+
+app.get('/*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
